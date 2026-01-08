313760c6a7dabf525d5e506cd312d7940951ebb3
root <root@common-portal.nsdb.com>
2026-01-08 05:05:22 +0000

    Remove duplicate '+' from Add Business Account menu item

--- src/resources/views/components/sidebar-menu.blade.php
+++ src/resources/views/components/sidebar-menu.blade.php
@@ -41,7 +41,7 @@
         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
         </svg>
-        + Add Business Account
+        Add Business Account
     </a>
 
     <div class="my-4 border-t" style="border-color: var(--sidebar-hover-background-color);"></div>
