c0017b867aaecddef445f81744d579caee391386
root <root@common-portal.nsdb.com>
2026-01-08 22:24:47 +0000

    Wrap all sidebar menu items with __translator()

--- src/resources/views/components/sidebar-menu.blade.php
+++ src/resources/views/components/sidebar-menu.blade.php
@@ -13,7 +13,7 @@
                   d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"/>
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
         </svg>
-        Administrator
+        {{ __translator('Administrator') }}
     </a>
     <div class="my-4 border-t" style="border-color: var(--sidebar-hover-background-color);"></div>
     @endif
@@ -21,7 +21,7 @@
     {{-- Account Selector --}}
     @auth
     <div class="mb-4">
-        <label class="block text-xs uppercase tracking-wide opacity-60 mb-2">Active Account</label>
+        <label class="block text-xs uppercase tracking-wide opacity-60 mb-2">{{ __translator('Active Account') }}</label>
         <select id="account-selector" 
                 onchange="switchAccount(this.value)"
                 class="w-full px-3 py-2 rounded-md text-sm border-0 focus:ring-2 focus:ring-opacity-50"
@@ -41,7 +41,7 @@
         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
         </svg>
-        Add Business Account
+        {{ __translator('Add Business Account') }}
     </a>
 
     <div class="my-4 border-t" style="border-color: var(--sidebar-hover-background-color);"></div>
@@ -60,7 +60,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
             </svg>
-            Account
+            {{ __translator('Account') }}
         </a>
         @endif
 
@@ -73,7 +73,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"/>
             </svg>
-            Dashboard
+            {{ __translator('Dashboard') }}
         </a>
         @endif
 
@@ -86,7 +86,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
             </svg>
-            Transactions
+            {{ __translator('Transactions') }}
         </a>
         @endif
 
@@ -99,7 +99,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
             </svg>
-            Billing
+            {{ __translator('Billing') }}
         </a>
         @endif
 
@@ -112,7 +112,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"/>
             </svg>
-            Developer
+            {{ __translator('Developer') }}
         </a>
         @endif
 
@@ -125,7 +125,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
             </svg>
-            Team
+            {{ __translator('Team') }}
         </a>
         @endif
 
@@ -141,7 +141,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                   d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
         </svg>
-        My Profile
+        {{ __translator('My Profile') }}
     </a>
 
     {{-- Support (optional module) --}}
@@ -153,7 +153,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                   d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"/>
         </svg>
-        Support
+        {{ __translator('Support') }}
     </a>
     @endif
 
@@ -167,7 +167,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                       d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
             </svg>
-            Exit
+            {{ __translator('Exit') }}
         </button>
     </form>
     @endauth
@@ -181,7 +181,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                   d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"/>
         </svg>
-        Login / Register
+        {{ __translator('Login / Register') }}
     </a>
     @endguest
 
